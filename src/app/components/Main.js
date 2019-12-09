3ce756dbba1698ddcee48b65db553c8c7ee0a35f
miljkovicaleksandar <fazzon96@gmail.com>
2019-12-09 03:32:16 +0100

    removed unnecessary imports

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -3,7 +3,7 @@ import User from './User';
 import UserCard from './UserCard';
 import Search from './Search';
 import Loader from './Loader'
-import { Switch, Route } from 'react-router-dom';
+
 
 
 
